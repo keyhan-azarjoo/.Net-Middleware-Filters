60d6d0d01e7dbb41744e16ec8426275e8605cabb
keyhan_azarjoo <keyhan.azarjoo@seamlessdigital.com>
2024-05-31 17:12:23 +0100

    dotnet files

--- Middleware_And_Filter/obj/Debug/net8.0/Middleware_And_Filter.AssemblyInfo.cs
+++ Middleware_And_Filter/obj/Debug/net8.0/Middleware_And_Filter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Middleware_And_Filter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cc37fd53da193ee2e4c3387bff8771c4cf35109")]
 [assembly: System.Reflection.AssemblyProductAttribute("Middleware_And_Filter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Middleware_And_Filter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
