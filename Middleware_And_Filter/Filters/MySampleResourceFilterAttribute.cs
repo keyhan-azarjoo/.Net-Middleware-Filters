2bc94041d2b7c71463109c875549e7b2cf2156a3
keyhan_azarjoo <keyhan.azarjoo@seamlessdigital.com>
2024-06-03 12:51:58 +0100

    final

--- Middleware_And_Filter/Filters/MySampleResourceFilterAttribute.cs
+++ Middleware_And_Filter/Filters/MySampleResourceFilterAttribute.cs
@@ -16,14 +16,14 @@ namespace Middleware_And_Filter.Filters
         {
             Console.WriteLine($"This Filter Executed Befor {_callername}");
 
-            // If you want to return the value you can do in any were like here.
-            // as this is the first class and you have the result in your cache you can return it here at the first level.
-            // To do that, you can write your result in your result content.
-
-            context.Result = new ContentResult()
-            {
-                Content = "This is my result and the request is npot going further."
-            };
+            //// If you want to return the value you can do in any were like here.
+            //// as this is the first class and you have the result in your cache you can return it here at the first level.
+            //// To do that, you can write your result in your result content.
+            ////
+            //context.Result = new ContentResult()
+            //{
+            //    Content = "This is my result and the request is npot going further."
+            //};
 
 
         }
