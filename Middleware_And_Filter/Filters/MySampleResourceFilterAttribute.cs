c6a5a0d2e29ad10208ef03ac6a28b9b694c950a8
keyhan_azarjoo <keyhan.azarjoo@seamlessdigital.com>
2024-06-03 12:24:53 +0100

    Using dependency injection

--- Middleware_And_Filter/Filters/MySampleResourceFilterAttribute.cs
+++ Middleware_And_Filter/Filters/MySampleResourceFilterAttribute.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Middleware_And_Filter.Filters
 {
@@ -14,6 +15,17 @@ namespace Middleware_And_Filter.Filters
         public void OnResourceExecuting(ResourceExecutingContext context)
         {
             Console.WriteLine($"This Filter Executed Befor {_callername}");
+
+            // If you want to return the value you can do in any were like here.
+            // as this is the first class and you have the result in your cache you can return it here at the first level.
+            // To do that, you can write your result in your result content.
+
+            context.Result = new ContentResult()
+            {
+                Content = "This is my result and the request is npot going further."
+            };
+
+
         }
          
         public void OnResourceExecuted(ResourceExecutedContext context)
